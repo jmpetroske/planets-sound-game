26ca93994a85b7b6915524c88b88c8d990872554
Joseph Petroske <jmpetroske@gmail.com>
2018-10-30 11:13:09 -0700

    working on passing data with osc

--- OSCManager.gd
+++ OSCManager.gd
@@ -23,6 +23,7 @@ func send_osc_ready():
 
 func send_osc_update(global_p, relative_p, v):
 	msg_address("/planet/update")
+	msg_add_int(get_parent().uid)
 	msg_add_v2(global_p)
 	msg_add_v2(relative_p)
 	msg_add_v2(v)
